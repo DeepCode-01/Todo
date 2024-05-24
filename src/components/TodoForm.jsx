fac77a9d0553fefbf3c0084d899ac785e14a8344
deepali verma <deepaliverma064@gamil.com>
2024-05-24 15:22:18 +0530

    first commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoForm = () => {
+  return (
+    <div>
+       <form action="" className='form'>
+        <input type="text" placeholder='Enter your todo......' />
+       </form>
+    </div>
+  )
+}
+
+export default TodoForm
